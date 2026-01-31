7eac81ad0afb8d9af1fc4ae5d7bc0cba9b2bf9a7
phil-cork-epic <prcork@gmail.com>
2026-01-31 16:49:16 -0500

    clean up imports

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
-import pandas as pd
 from streamlit_autorefresh import st_autorefresh
-from utils.data_loader import most_frequent_value, run_data_pipeline
-from utils.charts import donut_chart, bar_chart, smart_metric
+from utils.data_loader import run_data_pipeline
 from tabs import stats, data_demo
 
 st.set_page_config(layout="wide")
